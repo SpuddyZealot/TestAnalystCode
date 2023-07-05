27abdb77ac9a75df3e0811637d31db1ba2a3d27d
SpuddyZealot <davidminer22@gmail.com>
2023-07-05 16:55:45 +1200

    Tests Updated

--- TurnUpPortalTest/TurnUpPortalTests/Program.cs
+++ TurnUpPortalTest/TurnUpPortalTests/Program.cs
@@ -7,36 +7,57 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        //Login Test
+
+        //Open New Chrome Browser and Navigate to TurnUp Login Page
         IWebDriver driver = new ChromeDriver();
         driver.Manage().Window.Maximize();
-
         driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
 
+        //Enter Login Credentials
         IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
         usernameTextbox.SendKeys("hari");
 
         IWebElement PasswordTextbox = driver.FindElement(By.Id("Password"));
         PasswordTextbox.SendKeys("123123");
 
+        //Click Login Button
         IWebElement LoginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
         LoginButton.Click();
 
+        //Test
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User has logged in successfully.");
+
+        }
+        else
+        {
+            Console.WriteLine("User has failed to log in");
+        }
+
+        //Create New Entry Test
+
+        //Navigate to Time and Materials Tab
         IWebElement AdminButton = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
         AdminButton.Click();
-
         IWebElement TimeMatButton = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
         TimeMatButton.Click();
 
+        //Create New Entry
         IWebElement NewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
         NewButton.Click();
 
-        IWebElement CodeField = driver.FindElement(By.Id("Code"));
-        CodeField.Click();
-        CodeField.SendKeys("EditTest");
+        //Add Information to New Entry
+        IWebElement codeField = driver.FindElement(By.Id("Code"));
+        codeField.Click();
+        codeField.SendKeys("Test");
 
-        IWebElement DescField = driver.FindElement(By.Id("Description"));
-        DescField.Click();
-        DescField.SendKeys("EditTest");
+        IWebElement descField = driver.FindElement(By.Id("Description"));
+        descField.Click();
+        descField.SendKeys("This is a test Entry, Delete after test");
 
         IWebElement pricePUField = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
         pricePUField.Click();
